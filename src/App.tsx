242d3965e98160cb052f0d28c3f9fd3da6f6c719
Vantonage <oshawot77@gmail.com>
2023-12-22 01:40:03 -0500

    Fixed error?

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { About } from "./pages/About"
-import { NavBar } from "./components/NavBar"
+import { NavBar } from "./components/Navbar"
 
 
 function App() {
